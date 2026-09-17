6cee831d0f52e4c98159da6ece5fc4718139dd3a
KhaosMagnus <khaos.magnus@gmail.com>
2026-09-17 03:18:30 -0600

    feat: export serial service

--- src/service/index.ts
+++ src/service/index.ts
@@ -12,4 +12,5 @@ export * from './use-master-seri';
 export * from './use-notification';
 export * from './use-ocg-font';
 export * from './use-preset-manager';
+export * from './use-serial';
 export * from './use-setting';
