2917bd44010797f743ca252b585382867e699875
KhaosMagnus <khaos.magnus@gmail.com>
2026-09-19 12:51:04 -0600

    refactor: classify custom outer foils

--- src/model/foil-finish-dye.tsx
+++ src/model/foil-finish-dye.tsx
@@ -278,6 +278,7 @@ export type FoilDictionary = {
     platinum: string,
     rainbow: string,
     grandMasterRare?: string,
+    yu3doh?: string,
 };
 const FoilPreview = styled.div`
     display: inline-block;
@@ -332,6 +333,15 @@ export const FoilMap = {
         )` }} />,
         isOption: true,
     },
+    yu3doh: {
+        name: 'yu3doh' as const,
+        color: '#111111',
+        preview: <FoilPreview style={{
+            backgroundColor: '#111111',
+            boxShadow: 'inset 0 0 0 1px #f1f1f1',
+        }} />,
+        isOption: true,
+    },
 };
 export const getFoilList = (dictionary?: FoilDictionary) => [
     {
@@ -350,16 +360,34 @@ export const getFoilList = (dictionary?: FoilDictionary) => [
         tooltip: dictionary?.grandMasterRare ?? 'Grand Master Rare',
         ...FoilMap.grandMasterRare,
     },
-    // {
-    //     tooltip: dictionary?.rainbow,
-    //     ...FoilMap.rainbow,
-    // },
+    {
+        tooltip: dictionary?.rainbow,
+        ...FoilMap.rainbow,
+    },
+    {
+        tooltip: dictionary?.yu3doh ?? 'YU3DOH',
+        ...FoilMap.yu3doh,
+    },
 ];
 export type Foil = ReturnType<typeof getFoilList>[0]['name'];
 
 export const GRAND_MASTER_RARE_FOIL: Foil = 'grand-master-rare';
-export const normalizeStandardFoil = (foil: Foil): Exclude<Foil, 'grand-master-rare'> =>
-    foil === GRAND_MASTER_RARE_FOIL ? 'normal' : foil;
+
+export const CUSTOM_OUTER_FOIL_ASSET_MAP = {
+    [GRAND_MASTER_RARE_FOIL]: 'frame/card-border-grand-master-rare.png',
+    rainbow: 'frame/card-border-rainbow.png',
+    yu3doh: 'frame/card-border-yu3doh.png',
+} as const;
+export type CustomOuterFoil = keyof typeof CUSTOM_OUTER_FOIL_ASSET_MAP;
+
+export const isCustomOuterFoil = (foil: Foil): foil is CustomOuterFoil =>
+    Object.prototype.hasOwnProperty.call(CUSTOM_OUTER_FOIL_ASSET_MAP, foil);
+
+export const getCustomOuterFoilAsset = (foil: Foil) =>
+    isCustomOuterFoil(foil) ? CUSTOM_OUTER_FOIL_ASSET_MAP[foil] : undefined;
+
+export const normalizeStandardFoil = (foil: Foil): Exclude<Foil, CustomOuterFoil> =>
+    isCustomOuterFoil(foil) ? 'normal' : foil;
 
 export type FrameDyeList = [
     topLeft: string,
